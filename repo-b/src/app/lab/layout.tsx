c725018201babd2e86d3dcca7cc20b8afca1a102
Paul Malmquist <paulmalmquist@gmail.com>
2026-03-30 21:05:24 -0400

    repe fix

--- repo-b/src/app/lab/layout.tsx
+++ repo-b/src/app/lab/layout.tsx
@@ -1,10 +1,13 @@
 import AppShell from "@/components/AppShell";
 import { EnvProvider } from "@/components/EnvProvider";
+import { BusinessProvider } from "@/lib/business-context";
 
 export default function LabLayout({ children }: { children: React.ReactNode }) {
   return (
     <EnvProvider>
-      <AppShell>{children}</AppShell>
+      <BusinessProvider>
+        <AppShell>{children}</AppShell>
+      </BusinessProvider>
     </EnvProvider>
   );
 }
